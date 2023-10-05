74bff7f4412d8e0797af3031f46b5e13a209edda
Batwan01 <wldhks1120@naver.com>
2023-10-05 21:53:44 +0900

    Changes

---
+++ src/test/java/testserver/cd/testproject/TestprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package testserver.cd.testproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
