83efe2f3f03f7d33aec6b8c5af6a1e5681ebc89a
Batwan01 <wldhks1120@naver.com>
2023-10-06 19:48:34 +0900

    Changes

--- src/main/java/testserver/cd/testproject/model/InfoVO.java
+++ src/main/java/testserver/cd/testproject/model/InfoVO.java
@@ -1,5 +1,6 @@
 package testserver.cd.testproject.model;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class InfoVO {
     private String id;
     private String name;
@@ -13,15 +14,14 @@ public class InfoVO {
         return id;
     }
     public void setId(String id) {
-        if(id.length()<2)
-            System.out.println("sdsd");
-            //throw new IllegalArgumentException("2글자 이상이어야 합니다.");
         this.id = id;
     }
     public String getName() {
         return name;
     }
     public void setName(String name) {
+        if(id.length()<2)
+            throw new IllegalArgumentException("2글자 이상이어야 합니다.");
         this.name = name;
     }
 
@@ -36,6 +36,12 @@ public class InfoVO {
         return email;
     }
     public void setemail(String email) {
+        String regex = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(email);
+        if (!m.matches()) {
+            throw new IllegalArgumentException("이메일 형식 x");
+        }
         this.email = email;
     }
 
@@ -43,6 +49,16 @@ public class InfoVO {
         return password;
     }
     public void setpassword(String password) {
+        String regex = "^(?=.*[!@#$%^&*()])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[0-9]).*$";
+        /*(?=.*[!@#$%^&*()]): 특수문자
+        (?=.*[A-Z]): 대문자
+        (?=.*[a-zA-Z]): 영문자
+        (?=.*[0-9]): 숫자*/
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("비밀번호틀림");
+        }
         this.password = password;
     }
 
@@ -50,15 +66,24 @@ public class InfoVO {
         return phone;
     }
     public void setphone(String phone) {
+        if (!phone.matches("[0-9]+") || phone.length()>11 || phone.length()<10) {
+            throw new IllegalArgumentException("비밀번호틀림");
+        }
         this.phone = phone;
+
     }
 
     public String getjob() {
         return job;
     }
     public void setjob(String job) {
-        this.job = job;
+        if (job.isEmpty()) {
+            this.job = "no";
+        } else {
+            this.job = job;
+        }
     }
+
 
 }
 
